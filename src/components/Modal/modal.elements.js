c613da72f99772abf9c5e0c063725a7f5c425e83
psarks <paulinaalesarquis@gmail.com>
2021-02-03 17:29:58 -0800

    modal created

--- src/components/Modal/modal.elements.js
+++ src/components/Modal/modal.elements.js
@@ -3,33 +3,62 @@ import { MdClose } from 'react-icons/md'
 
 
 export const Background = styled.div`
-    width: 100%;
-    height: 100%;
-    background: rgba(0,0,0,0.8);
-    position: fixed;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  width: 100%;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.8);
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 0px;
+  left: 0;
+  z-index: 999;
     `;
 
 export const ModalWrapper = styled.div`
     width: 800px;
-    height: 500px;
-    box-shadow: 0 5px 16px rgba(0,0,0,0.2);
+    height: 610px;
+    box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
     background: #fff;
     color: #000;
     display: grid;
     grid-template-columns: 1fr 1fr;
-    position: relative; 
+    position: relative;
+    top: 10px;
     z-index: 10;
     border-radius: 10px;
+
+
+    @media screen and (max-width: 968px) {
+        height: 90%;
+        left: 20px;
+        
+    }
+   
+    /*width: 70vw;
+    height: 65%;
+    box-shadow: 0 150px 200px rgba(0,0,0,0.8);
+    background: #fff;
+    color: #000;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    border-radius: 10px;
+    position: absolute;
+    top: 100px;
+    left: 210px;
+    bottom: 0;
+    z-index: 10;
+    
+*/
 `;
 
 export const ModalImg = styled.img`
     width: 100%;
     height: 100%;
+    
     border-radius: 10px 0 0 10px;
     background: #000;
+    
 
 `;
 
@@ -41,6 +70,16 @@ export const ModalContent = styled.div`
     line-height: 1.8;
     color: #141414;
 
+    p {
+    margin-bottom: 1rem;
+  }
+  button {
+    padding: 10px 24px;
+    background: #141414;
+    color: #fff;
+    border: none;
+  }
+
 `;
 
 export const CloseModalButton = styled(MdClose)`
@@ -51,4 +90,5 @@ export const CloseModalButton = styled(MdClose)`
     height: 32px;
     padding: 0;
     z-index: 10;
+    cursor: pointer;
 `;
--- src/components/Modal/modal.js
+++ src/components/Modal/modal.js
@@ -1,20 +1,59 @@
-import React from 'react';
+import React, {useRef, useEffect, useCallback} from 'react';
+import { useSpring, animated } from 'react-spring'
 import {Background, ModalWrapper, ModalImg, ModalContent, CloseModalButton} from './modal.elements'
+import penguin from '../../images/penguin.png'
+
 
 export const Modal = ({ showModal, setShowModal }) => {
+    const modalRef = useRef();
+
+    const animation = useSpring({
+            config: {
+            duration: 500
+        },
+            opacity: showModal ? 1 : 0,
+            transform: showModal ? `translateY(0%)` : `translateY(-100%)`
+    });
+
+  const closeModal = e => {
+            if (modalRef.current === e.target) {
+            setShowModal(false);
+        }
+    };
+
+  const keyPress = useCallback(
+        e => {
+                if (e.key === 'Escape' && showModal) {
+                setShowModal(false);
+                console.log('I pressed');
+            }
+        },
+         [setShowModal, showModal]
+    );
+
+  useEffect(
+        () => {
+            document.addEventListener('keydown', keyPress);
+            return () => document.removeEventListener('keydown', keyPress);
+        },
+        [keyPress]
+    );
+    
     return(  
     
     <> 
         {showModal ? ( 
             <Background>
+                <animated.div style={animation}>
                 <ModalWrapper showModal={showModal}>
-                    <ModalImg src={require('./cv2020.pdf')} alt='resume' />
+                    <ModalImg src={penguin} alt='resume' />
                     <ModalContent>
                         hi
                     </ModalContent>
                     <CloseModalButton araia-label='Close modal' onClick={() => setShowModal(prev => !prev)}/>
 
                 </ModalWrapper>
+                </animated.div>
             </Background>
         ):null}
     </>
