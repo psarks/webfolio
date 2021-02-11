51f217dd1e2f1c67af04e92544d81bcfc85cb10e
psarks <paulinaalesarquis@gmail.com>
2021-02-10 18:35:08 -0800

    added InfoBucketlist to index

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ export { default as Navbar } from './Navbar/Navbar';
 export { default as InfoSection } from './InfoSection/infoSection';
 export {default  as InfoPhoto } from './InfoPhoto/infoPhoto'
 export { default as InfoCV } from './InfoCV/infoCV'
+export { default as InfoBucketlist } from './InfoBucketlist/InfoBucketlist'
 export {default as Footer } from './Footer/Footer';
 export {default as ImageSlider } from './ImageSlider/ImageSlider'
 
