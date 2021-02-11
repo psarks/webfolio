c8956266f14ce559f2c8ac41383acced64a75a7e
psarks <paulinaalesarquis@gmail.com>
2021-02-10 18:39:56 -0800

    bucketlist data

---
+++ src/pages/bucketlistpage/Data.js
@@ -0,0 +1,11 @@
+
+
+export const bucketData = {
+     
+    lightTextDesc: false , 
+    description: 'I strive to better myself as much as possible even when it seems to be impossible.' , 
+    headline: 'Bucketlist',
+    lightText: false,
+    
+    start: 'false'
+};
