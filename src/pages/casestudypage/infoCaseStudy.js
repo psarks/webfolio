310b48f03aa8ba4056f229b128830900648e7945
psarks <paulinaalesarquis@gmail.com>
2021-02-11 16:52:24 -0800

    created case study page

---
+++ src/pages/casestudypage/infoCaseStudy.js
@@ -0,0 +1,25 @@
+import React from "react";
+import CaseStudy from "../casestudypage/CaseStudy";
+import casestudyData from "../casestudypage/casestudy.json";
+
+const infoCaseStudy = () => {
+  return (
+    <>
+      <CaseStudy.Container>
+        {casestudyData.map((item) => (
+          <CaseStudy key={item.id} direction={item.direction}>
+            <CaseStudy.Pane>
+              <CaseStudy.Title>{item.title}</CaseStudy.Title>
+              <CaseStudy.SubTitle>{item.subTitle}</CaseStudy.SubTitle>
+            </CaseStudy.Pane>
+            <CaseStudy.Pane>
+              <CaseStudy.Image src={item.image} alt={item.alt} />
+            </CaseStudy.Pane>
+          </CaseStudy>
+        ))}
+      </CaseStudy.Container>
+    </>
+  );
+};
+
+export default infoCaseStudy;
